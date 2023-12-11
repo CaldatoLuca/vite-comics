89e9ab783d72006290a91c536d3820ff1192118c
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-11 18:00:39 +0100

    feat: v-for sui comics

---
+++ src/components/CommonElement.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "comic",
+};
+</script>
+<template>
+  <div class="element">
+    <div class="image">img</div>
+    <div class="text">text</div>
+  </div>
+</template>
+<style lang="scss" scoped></style>
